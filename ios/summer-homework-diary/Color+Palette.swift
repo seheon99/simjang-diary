be21f18211b2cf2357ebbf65343a3586520ea165
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2026-08-08 22:35:52 +0900

    feat: add taupe color palette

---
+++ ios/summer-homework-diary/Color+Palette.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension Color {
+    static let taupe50 = Color(.sRGB, red: 0.985423913, green: 0.980824680, blue: 0.976225294)
+    static let taupe100 = Color(.sRGB, red: 0.952960975, green: 0.945648099, blue: 0.945647330)
+    static let taupe200 = Color(.sRGB, red: 0.911329318, green: 0.894144330, blue: 0.889850224)
+    static let taupe300 = Color(.sRGB, red: 0.846577156, green: 0.823703940, blue: 0.816062769)
+    static let taupe400 = Color(.sRGB, red: 0.670328428, green: 0.627624946, blue: 0.612068664)
+    static let taupe500 = Color(.sRGB, red: 0.486006670, green: 0.427566738, blue: 0.404176327)
+    static let taupe600 = Color(.sRGB, red: 0.355876477, green: 0.309805296, blue: 0.294461928)
+    static let taupe700 = Color(.sRGB, red: 0.278168555, green: 0.235815193, blue: 0.224271151)
+    static let taupe800 = Color(.sRGB, red: 0.168641792, green: 0.141818078, blue: 0.134151739)
+    static let taupe900 = Color(.sRGB, red: 0.113655549, green: 0.093880630, blue: 0.085957216)
+    static let taupe950 = Color(.sRGB, red: 0.046956695, green: 0.039357738, blue: 0.035540755)
+}
