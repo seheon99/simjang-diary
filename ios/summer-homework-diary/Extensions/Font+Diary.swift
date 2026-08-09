1cd66594f1d326e9956f7b774f9652069997ca1a
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2026-08-09 17:25:08 +0900

    deps: Add font extension

---
+++ ios/summer-homework-diary/Extensions/Font+Diary.swift
@@ -0,0 +1,40 @@
+//
+//  Font+Diary.swift
+//  summer-homework-diary
+//
+//  Created by 유세헌 on 8/9/26.
+//
+
+import SwiftUI
+
+extension Font {
+    static let diaryTitle = Font.custom(
+        "MaruBuri-Bold",
+        size: 28,
+        relativeTo: .title
+    )
+
+    static let diaryHeadline = Font.custom(
+        "MaruBuri-SemiBold",
+        size: 18,
+        relativeTo: .headline
+    )
+
+    static let diaryBody = Font.custom(
+        "NanumBaReunHiPi",
+        size: 17,
+        relativeTo: .body
+    )
+    
+    static let diaryComment = Font.custom(
+        "NanumSeongSirCe",
+        size: 17,
+        relativeTo: .body
+    )
+
+    static let diaryCaption = Font.custom(
+        "MaruBuri-Light",
+        size: 13,
+        relativeTo: .caption
+    )
+}
